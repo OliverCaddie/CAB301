2963753728f92f9ca979b429e9a9bf6769e9b13d
Oliver Caddie <oliver.caddie@gmail.com>
2019-04-11 02:21:20 +1000

    added report

--- Assignment1.java
+++ Assignment1.java
@@ -4,66 +4,91 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Assignment1 {
 	
 	static Random r = new Random();
-	static int count = 0;
-
-	public static void main(String[] args) throws IOException {
-		int numResults = 100;
+	static int count = -1;
+	static int equal = -1;
+	static int index = -1;
+	
+	public static void main(String[] args) {
+		int runType = 1; // 1 for experiment, 0 for test
+		if (runType == 1) experiment(100, 10, 100); // numValues, numReapeats, jumpSize
+		else System.out.println(test() ? "success" : "fail");
+	}
+	
+	static boolean test() {
+		int maxLength = 20;
 		int numRepeats = 10;
-		int jumpSize = 100;
-		StringBuilder results = new StringBuilder();
-		StringBuilder data = new StringBuilder();
-		StringBuilder dataUnordered = new StringBuilder();
-		int n;
 		int[] input;
+		int result;
+		for (int n = 1; n <= maxLength; n++) {
+			for (int i = 1; i <= numRepeats; i++) {
+				input = generate(n);
+				result = bruteForceMedian(input);
+				Arrays.sort(input);
+				if (result != input[(int) Math.ceil(((double) n)/2) - 1]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	static void experiment(int numValues, int numRepeats, int jumpSize) {
+		StringBuilder results = new StringBuilder();
+		int n;
+		int[] input = null;
 		long t1, t2;
+		int result;
 		
 		results.append("n");
 		results.append("\t");
 		results.append("count");
 		results.append("\t");
 		results.append("time");
+		results.append("\t");
+		results.append("index");
+		results.append("\t");
+		results.append("equal");
+		results.append("\t");
+		results.append("result");
 		results.append("\n");
 		
-		for (int i = 0; i < numResults; i++) {
+		for (int i = 0; i < numValues; i++) {
+			n = (i+1)*jumpSize;
 			for (int j = 0; j < numRepeats; j++) {
-				n = (i+1)*jumpSize;
 				count = 0;
 				input = generate(n);
 				t1 = System.currentTimeMillis();
-				bruteForceMedian(input);
+				result = bruteForceMedian(input);
 				t2 = System.currentTimeMillis();
-				
-				data.append(Integer.toString(n));
-				for (int k = 0; k < n; k++) {
-					data.append("\t");
-					data.append(Integer.toString(input[k]));
-					dataUnordered.append(Integer.toString(input[k]));
-					dataUnordered.append("\n");
-				}
-				data.append("\n");
-				
 				results.append(Integer.toString(n));
 				results.append("\t");
 				results.append(Integer.toString(count));
 				results.append("\t");
 				results.append(Long.toString(t2 - t1));
+				results.append("\t");
+				results.append(Integer.toString(index));
+				results.append("\t");
+				results.append(Integer.toString(equal));
+				results.append("\t");
+				results.append(Integer.toString(result));
 				results.append("\n");
 			}
+			System.out.println(input.length);
 		}
 		
-		
-		Path dPath = Paths.get("src/cab301/data.txt");
-		Path d2Path = Paths.get("src/cab301/dataNoFormat.txt");
 		Path rPath = Paths.get("src/cab301/results.txt");
+		try {
+			Files.write(rPath, results.toString().getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
-		Files.write(dPath, data.toString().getBytes());
-		Files.write(d2Path, dataUnordered.toString().getBytes());
-		Files.write(rPath, results.toString().getBytes());
 	}
 
 	static int bruteForceMedian(int[] a) {
@@ -80,16 +105,38 @@ public class Assignment1 {
 				}
 			}
 			if (numSmaller < k && k <= numSmaller + numEqual) {
+				equal = numEqual;
+				index = i;
 				return a[i];
 			}
 		}
 		return -1;
 	}
 	
+	static int medTest(int[] a) {
+		int n = a.length;
+		int k = (int) Math.ceil(((double) n)/2);
+		int numSmaller = 0;
+		int numEqual = 0;
+		int i = 0;
+		for (int j = 0; j < n; j++) {
+			if (++count > 0 && a[j] < a[i]) {
+				numSmaller++;
+			} else if (++count > 0 && a[j] == a[i]) {
+				numEqual++;
+			}
+		}
+		if (numSmaller < k && k <= numSmaller + numEqual) {
+			return a[i];
+		}
+		
+		return -1;
+	}
+	
 	static int[] generate(int n) {
 		int[] a = new int[n];
 		for (int i = 0; i < a.length; i++) {
-			a[i] = r.nextInt();
+			a[i] = r.nextInt(n); 
 		}
 		return a;
 	}
